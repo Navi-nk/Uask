f2f1670735ccf8b3e6c1ce1d0a7718a199b51c2b
ethiraj <ethirajsrinivasan@gmail.com>
2017-02-18 22:54:08 +0800

    recylerview inital for question

--- app/src/main/java/com/ideascontest/navi/uask/MainCanvas.java
+++ app/src/main/java/com/ideascontest/navi/uask/MainCanvas.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -28,6 +30,10 @@ public class MainCanvas extends AppCompatActivity
     private static final String TAG = "Main Canvas";
     // Session Manager Class
     SessionManager _session;
+
+    private RecyclerView mainQuestionAnswerList;
+    private static final int NUM_LIST_ITEMS = 100;
+    private MainQuestionAnswerAdapter mQuestionAnswerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,15 +92,6 @@ public class MainCanvas extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -116,6 +113,36 @@ public class MainCanvas extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        mainQuestionAnswerList = (RecyclerView) findViewById(R.id.question_top_answer_recylerview);
+
+        /*
+         * A LinearLayoutManager is responsible for measuring and positioning item views within a
+         * RecyclerView into a linear list. This means that it can produce either a horizontal or
+         * vertical list depending on which parameter you pass in to the LinearLayoutManager
+         * constructor. By default, if you don't specify an orientation, you get a vertical list.
+         * In our case, we want a vertical list, so we don't need to pass in an orientation flag to
+         * the LinearLayoutManager constructor.
+         *
+         * There are other LayoutManagers available to display your data in uniform grids,
+         * staggered grids, and more! See the developer documentation for more details.
+         */
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mainQuestionAnswerList.setLayoutManager(layoutManager);
+
+        /*
+         * Use this setting to improve performance if you know that changes in content do not
+         * change the child layout size in the RecyclerView
+         */
+        mainQuestionAnswerList.setHasFixedSize(true);
+
+        // COMPLETED (13) Pass in this as the ListItemClickListener to the MainQuestionAnswerAdapter constructor
+        /*
+         * The MainQuestionAnswerAdapter is responsible for displaying each item in the list.
+         */
+        mQuestionAnswerAdapter = new MainQuestionAnswerAdapter(NUM_LIST_ITEMS);
+        mainQuestionAnswerList.setAdapter(mQuestionAnswerAdapter);
     }
 
     @Override

53df7b3890e4569fa9ac5f7a47ef4361e6d0bbd6
ethiraj <ethirajsrinivasan@gmail.com>
2017-02-19 22:29:07 +0800

    answer flow completed

--- app/src/main/java/com/ideascontest/navi/uask/NetworkUtils.java
+++ app/src/main/java/com/ideascontest/navi/uask/NetworkUtils.java
@@ -17,8 +17,13 @@ package com.ideascontest.navi.uask;
 
 import android.net.Uri;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -26,6 +31,7 @@ import java.util.Scanner;
 
 
 import android.net.Uri;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,6 +39,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * These utilities will be used to communicate with the network.
@@ -45,9 +53,13 @@ public class NetworkUtils {
     final static String GET_ALL_QUESTIONS = BASE_URL+"/qfeed/getfeed";
     final static String GET_ALL_ANSWERS = BASE_URL+"/qfeed/getans";
     final static String GET_ALL_QUESTION_FOR_CAT = BASE_URL+"/qfeed/getcatfeed";
+    final static String POST_ANSWER = BASE_URL+"/qfeed/ansques";
 
     final static String PARAM_QUESTION = "question";
     final static String PARAM_CATEGORY="category";
+    final  static String PARAM_QUESTION_ID = "questionId";
+    final static String PARAM_USER_ID = "userId";
+    final static  String ANSWER ="answer";
 
     /*
      * The sort field. One of stars, forks, or updated.
@@ -61,6 +73,23 @@ public class NetworkUtils {
     public static URL buildUrl(String BaseURL,String param,String paramvalue) {
         Uri builtUri = Uri.parse(BaseURL).buildUpon()
                 .appendQueryParameter(param, paramvalue)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static URL buildUrlToPostAnswer(String BaseURL,String user_id,String user_value,String question_id,String question_value,String answer,String answer_value) {
+        Uri builtUri = Uri.parse(BaseURL).buildUpon()
+                .appendQueryParameter(user_id, user_value)
+                .appendQueryParameter(question_id, question_value)
+                .appendQueryParameter(answer, answer_value)
                 .build();
 
         URL url = null;
@@ -99,4 +128,29 @@ public class NetworkUtils {
             urlConnection.disconnect();
         }
     }
+
+    public static String putResponseFromHttpUrl(URL url) throws IOException {
+        String response="";
+        try {
+            Log.d("url check",url.toString());
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            OutputStream os = urlConnection.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(os, "UTF-8"));
+        writer.write(urlConnection.toString());
+
+        writer.flush();
+        writer.close();
+        os.close();
+        int responseCode=urlConnection.getResponseCode();
+        Log.d("responsecode",String.valueOf(responseCode));
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+    return response;
+
+
+}
 }
